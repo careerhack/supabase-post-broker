3b5e98af96c21dc7bd2f87493e55823faa2523f2
Aaron DeVera <aaron@backchannel.re>
2022-01-19 22:09:58 -0800

    updated pull instructions

--- server.py
+++ server.py
@@ -74,7 +74,7 @@ def read_root():
 async def gitUpdate(request: Request, body: RowData, auth: Optional[str] = None):
     if auth:
         if auth == AUTHORIZATION_TOKEN:
-            os.system('cd /root/supabase-webhook-broker; git pull')
+            os.system('cd /root/supabase-webhook-broker && git pull')
             return JSONResponse({'status':200})
 
 @app.post('/api/v1/function/extractAndInsertURL')
